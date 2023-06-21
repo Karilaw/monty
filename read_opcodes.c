2b43afe2505f14ae3fcf52d2e9a2e79ee04b5228
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-21 07:10:36 -0700

    add nop func

--- read_opcodes.c
+++ read_opcodes.c
@@ -44,6 +44,10 @@ void process_opcodes(FILE *fp, stack_t **stack)
 		{
 			add(stack, line_number);
 		}
+		else if (strcmp(opcode, "nop") == 0)
+		{
+			nop(stack, line_number);
+		}
 		else
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n",

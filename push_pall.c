e85165c25473c495c8268de4973af12e73621e9f
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-21 04:48:53 -0700

    updated push and pall func

---
+++ push_pall.c
@@ -0,0 +1,62 @@
+#include "monty.h"
+/**
+ * push - pushes an element to the stack
+ * @stack: double pointer to the top of the stack
+ * @line_number: line number of the opcode in the file
+ * @n_str: string representing the integer to be pushed onto the stack
+ * Description: If n_str is not an integer or if there is no argument given,
+ * print an error message and exit with a failure status.
+ */
+void push(stack_t **stack, unsigned int line_number, char *n_str)
+{
+	stack_t *new_node;
+	int n, i;
+
+	if (n_str == NULL)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 0; n_str[i]; i++)
+		if (!isdigit(n_str[i]) && n_str[i] != '-')
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+
+	n = atoi(n_str);
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	*stack = new_node;
+}
+
+/**
+ * pall - prints all the values on the stack
+ * @stack: double pointer to the top of the stack
+ * @line_number: line number of the opcode in the file
+ * Description: If the stack is not empty print elements
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	(void)line_number;
+
+	temp = *stack;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}

362a9cccce25b5f487cb0c1bb39ee878522db4b9
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-21 08:46:45 -0700

    second commit to test

--- 8-mul.c
+++ 8-mul.c
@@ -1,24 +1,23 @@
 #include "monty.h"
 
 /**
- * mul_stack - multiplies the second top element of the stack
- * by the top element
+ * mul - multiplies the second top element of the stack with
+ * the top element of the stack
  * @stack: double pointer to the top of the stack
  * @line_number: line number of the opcode in the file
- * Description: If the stack contains less than two elements, print an error
- * message and exit with a failure status.
+ * Description: If the stack contains less than two elements,
+ * print an error message.
  */
-
-void mul_stack(stack_t **stack, unsigned int line_number)
+void mul(stack_t **stack, unsigned int line_number)
 {
-	int result;
+	int product;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: cannot mul, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	result = (*stack)->next->n * (*stack)->n;
+	product = (*stack)->next->n * (*stack)->n;
 	pop(stack, line_number);
-	(*stack)->n = result;
+	(*stack)->n = product;
 }
